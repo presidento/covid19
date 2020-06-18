062ac22e0b9d934d78f090c035b882c04d8e915b
Máté Farkas <mate.farkas@oneidentity.com>
2020-06-18 11:24:04 +0200

    Small fixes around upgrade and run script

--- upgrade-and-run.sh
+++ upgrade-and-run.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
-git fetch
+git fetch --quiet
 git reset --hard origin/master --quiet
-.venv/bin/python3 -m pip install -r requirements.txt
-.venv/bin/python3 collect.py
+
+cd COVID-19
+git pull --ff-only --quiet
+cd ..
+
+.venv/bin/python3 -m pip install -r requirements.txt --quiet
+.venv/bin/python3 collect.py --quiet
