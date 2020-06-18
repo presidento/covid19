0490c3616576a4681454c3b4521eab77e13cf968
Máté Farkas <mate.farkas@oneidentity.com>
2020-06-18 09:47:01 +0200

    Small fixes in runner scripts

--- run.bat
+++ run.bat
@@ -2,15 +2,26 @@
 
 echo Update code repository
 git pull --ff-only
-pause
+IF ERRORLEVEL 1 GOTO ERROR
+.venv\Scripts\python -m pip install -r requirements.txt -q
+IF ERRORLEVEL 1 GOTO ERROR
 
 echo Update Datase from GitHub
 cd COVID-19
-git pull
+git pull --ff-only
 cd ..
 
 echo Collect report
-python collect.py
+.venv\Scripts\python collect.py
+IF ERRORLEVEL 1 GOTO ERROR
 
 echo Done
+goto EXIT
+
+:ERROR
+echo.
+echo THERE WAS AN ERROR!!!
+echo.
+
+:EXIT
 pause
--- upgrade-and-run.sh
+++ upgrade-and-run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
 git fetch
 git reset --hard origin/master --quiet
+.venv/bin/python3 -m pip install -r requirements.txt
 .venv/bin/python3 collect.py
