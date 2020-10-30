32e8feb979d75cb1352aedb590a07d1562fc1bd2
Mate Farkas (mfarkas) <mate.farkas@quest.com>
2020-10-30 09:32:32 +0100

    Upgrade pip during automatic run

--- upgrade-and-run.sh
+++ upgrade-and-run.sh
@@ -8,5 +8,6 @@ git pull --ff-only --quiet
 cd ..
 
 rm -rf output/
+.venv/bin/python3 -m pip install --upgrade --quiet
 .venv/bin/python3 -m pip install -r requirements.txt --quiet
 .venv/bin/python3 collect.py --quiet
