a369b4168575ac9fe92a60174f6b52088b3229a8
Mate Farkas <Mate.Farkas@oneidentity.com>
2022-01-24 15:42:03 +0100

    Upgrade pip during bootstrap

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -7,4 +7,5 @@ If (-not (Test-Path "COVID-19")) {
 If (-not (Test-Path ".venv")) { py -3.7 -m venv .venv }
 .venv\Scripts\activate.ps1
 
-python -m pip --disable-pip-version-check -q install -r requirements.txt
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
